de385fe2fc679ac5011ed2c6b50745dd244ed70d
Fnoryn <fabien.ribeaucourt75@gmail.com>
2022-09-20 23:01:24 +0200

    ajout de limiter.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const checkEmail = require('../middleware/email-validator');
 const checkPassword = require('../middleware/password-validator');
+const limit = require('../middleware/limiter');
 
 
 // routes
 router.post('/signup', checkEmail, checkPassword, userCtrl.signup);
-router.post('/login',  userCtrl.login);
+router.post('/login', limit.max, userCtrl.login);
 
 // export
 module.exports = router;
